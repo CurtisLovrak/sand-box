5826f76c29bd0b8f9d87eb481bfa4de332695865
Curtis.Lovrak <curtislovrak@gmail.com>
2023-09-15 11:29:46 -0400

    added package-json

---
+++ algos.js
@@ -0,0 +1,22 @@
+// function twoSum(nums, target) {
+
+//     let output = [-1]
+
+//     while (output.length < 2) {
+//         output[0]++;
+//         let acc = (target - nums[output[0]])
+//         for (let i = acc[0]; i < nums.length; i++) {
+//             if (nums[output[0]] === acc) {
+//                 output.push(i)
+//             }
+//         }
+//     }
+//     return output
+// };
+
+// console.log(twoSum([2,7,11,15], 9))
+
+const test = "test";
+
+console.log(test)
+
